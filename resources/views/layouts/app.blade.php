6d9909732ccdf2746d33c5dafb23d46f433ee654
Pavel Gushchin <pavel_gushchin@mail.ru>
2021-03-21 02:44:49 +0700

    Add layout for creating tweets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,11 +27,7 @@
                 <div class="mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     <x-main-menu />
 
-                    <a href="/">
-                        <div class=" text-white bg-blue-600 text-center p-2 mt-3 rounded-2xl">
-                            Tweet
-                        </div>
-                    </a>
+                    <x-create-tweet-button />
                 </div>
             </header>
 
